bd364ca0a49a9997be8c455c8b74947424f5efec
oyoops <121578862+oyoops@users.noreply.github.com>
2023-07-13 23:57:52 -0400

    OOPS I RAAN FROM THE GRIND

--- spaceblazers.js
+++ spaceblazers.js
@@ -230,14 +230,14 @@ function draw() {
 		drawLabel("You are Damian Lillard, 11-time NBA All Star and greatest player in Portland franchise history.", width / 2, height / 2 + 100, textSize(), "center");
 		textStyle(NORMAL);
 		textSize(23);
-		drawLabel("Two weeks ago, you made a very polite request to management,", width / 2, height / 2 + 150, textSize(), "center");
-		drawLabel("but Crooked Joe Cronin had other ideas.", width / 2, height / 2 + 170, textSize(), "center");
+		drawLabel("Two weeks ago, you made a very polite request to management,", width / 2, height / 2 + 130, textSize(), "center");
+		drawLabel("but Crooked Joe Cronin had other ideas.", width / 2, height / 2 + 150, textSize(), "center");
 
-		textSize(20);
-		drawLabel("Destroy Joe Cronins by shooting at them with basketballs.", width / 2, height / 2 + 220, textSize(), "center");
-		drawLabel("If you make contact with him, you\'ll lose your Grind!", width / 2, height / 2 + 240, textSize(), "center");
-		drawLabel("Destroy 50 Joe Cronins before running out", width / 2, height / 2 + 280, textSize(), "center"); 
-		drawLabel("to force your way to the Miami Heat!", width / 2, height / 2 + 300, textSize(), "center");
+		textSize(18);
+		drawLabel("Destroy Joe Cronins by shooting at them with basketballs.", width / 2, height / 2 + 180, textSize(), "center");
+		drawLabel("If you make contact with him, you\'ll lose your Grind!", width / 2, height / 2 + 200, textSize(), "center");
+		drawLabel("Destroy 50 Joe Cronins before running out", width / 2, height / 2 + 240, textSize(), "center"); 
+		drawLabel("to force your way to the Miami Heat!", width / 2, height / 2 + 260, textSize(), "center");
 	
 	} else if (gameState === "play") {
         noCursor();
