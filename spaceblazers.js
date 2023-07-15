40d5f062ce5bd3fa39308e14b957fe8d70ddac62
oyoops <121578862+oyoops@users.noreply.github.com>
2023-07-14 20:03:21 -0400

    te

--- spaceblazers.js
+++ spaceblazers.js
@@ -296,7 +296,10 @@ function draw() {
 
 		}
 		// Draw the team logo in the center of the canvas
-		let logo = teamLogos[currentLevel.levelNumber - 1];
+		if (typeof myObject !== 'undefined' && myObject !== null) {
+			let logo = teamLogos[myObject.levelNumber - 1];
+		}
+		////let logo = teamLogos[currentLevel.levelNumber - 1];
 		image(logo, width / 2 - logo.width / 2, height / 2 - logo.height / 2, 50, 50);
 
 		// Display and move the spaceship
