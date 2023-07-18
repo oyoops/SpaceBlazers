e8a1627a94c109346d1a994e895bf4bd555f78d9
oyoops <121578862+oyoops@users.noreply.github.com>
2023-07-17 23:47:21 -0400

    assdick

--- spaceblazers.js
+++ spaceblazers.js
@@ -408,14 +408,14 @@ function draw() {
             image(croninImg, width / 4 - croninImg.width/2, height / 2 - 100, 150, 150);
         } else if (animationTime < 180) {
             textStyle(BOLD);
-            text("CRONIN:", 20, height / 2 + 30);
+            text("CRONIN:", width / 4, height / 2 + 30);
             text("DAME:", width / 2 + 10, height / 2 + 30);
             textStyle(NORMAL);
             image(lillardImg, width / 2 + 60 - lillardImg.width/2, height / 2 - 100, 150, 150);
             image(croninImg, width / 4 - croninImg.width/2, height / 2 - 100, 150, 150);
         } else if (animationTime < 270) {
             textStyle(BOLD);
-            text("CRONIN:", 20, height / 2 + 30);
+            text("CRONIN:", width / 4, height / 2 + 30);
             text("DAME:", width / 2 + 10, height / 2 + 30);
             textStyle(NORMAL);
             image(lillardImg, width / 2 + 60 - lillardImg.width/2, height / 2 - 100, 150, 150);
@@ -423,7 +423,7 @@ function draw() {
             dialogue1 = "I promise to bring star players...";
         } else if (animationTime < 360) {
             textStyle(BOLD);
-            text("CRONIN:", 20, height / 2 + 30);
+            text("CRONIN:", width / 4, height / 2 + 30);
             text("DAME:", width / 2 + 10, height / 2 + 30);
             textStyle(NORMAL);
             image(lillardImg, width / 2 + 60 - lillardImg.width/2, height / 2 - 100, 150, 150);
@@ -431,7 +431,7 @@ function draw() {
             dialogue2 = "Really? Wow, that will be so great!";
         } else if (animationTime < 450) {
             textStyle(BOLD);
-            text("CRONIN:", 20, height / 2 + 30);
+            text("CRONIN:", width / 4, height / 2 + 30);
             text("DAME:", width / 2 + 10, height / 2 + 30);
             textStyle(NORMAL);
             image(lillardImg, width / 2 + 60 - lillardImg.width/2, height / 2 - 100, 150, 150);
@@ -439,7 +439,7 @@ function draw() {
             dialogue3 = "Yeah, you won't believe who I bring in!";
         } else if (animationTime < 540) {
             textStyle(BOLD);
-            text("CRONIN:", 20, height / 2 + 30);
+            text("CRONIN:", width / 4, height / 2 + 30);
             text("DAME:", width / 2 + 10, height / 2 + 30);
             textStyle(NORMAL);
             image(lillardImg, width / 2 + 60 - lillardImg.width/2, height / 2 - 100, 150, 150);
@@ -448,7 +448,7 @@ function draw() {
             text("OK great. NBA Finals, here we come!", width / 2 + 10, height / 2 + 175);
         } else if (animationTime < 700) {
             textStyle(BOLD);
-            text("CRONIN:", 20, height / 2 + 30);
+            text("CRONIN:", width / 4, height / 2 + 30);
             text("DAME:", width / 2 + 10, height / 2 + 30);
             textStyle(NORMAL);
             image(lillardImg, width / 2 + 60 - lillardImg.width/2, height / 2 - 100, 150, 150);
